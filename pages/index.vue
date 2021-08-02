c718efbcb550f6ef92b923ab46fe858a4c2162a4
Deniz Gençtürk <denizar@gmail.com>
2021-08-02 18:21:54 +0200

    add isotope filtering to skills

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- Intro -->
-    <v-card class="mb-10">
+    <v-card class="mb-10 pb-5">
       <v-card-title>
         <v-avatar size="5em" class="mr-md-5 mx-auto ml-md-0 mb-3 mb-md-0">
           <v-img src="./logo_128.webp" />
@@ -22,60 +22,23 @@
         </div>
       </v-card-title>
       <v-card-text class="text-h6">
-        I picked up coding watching my freelancer work on an e-commerce & CRM web app for my liquid candle business <a href="https://decoralamps.com" target="_blank">Decora Lamps</a>. I fell in love with the ability to create things for people to use just from just the tip of my fingers.
+        I picked up coding watching my freelancer work on an e-commerce & CRM web app for my liquid candle business <a href="https://decoralamps.com" target="_blank">Decora Lamps</a>. I fell in love with the ability to create things just from the tip of my fingers and decided to focus most of my time into coding.
       </v-card-text>
       <v-card-text class="text-h6">
-        After my export business took a hit from ever changing customs regulations in Turkey, I decided to go full time into coding. I met a couple who owned <a href="https://suslusozluk.com" target="_blank">suslusozluk.com</a> which had over 1m monthly visitors, but an old code-base that couldn't handle the traffic. We dug in and I pushed my limits, re-doing the complete web-app in less than a year. The website now enjoys over 2.5m monthly users, and I maintain it to this day.
+        I met a couple who owned <a href="https://suslusozluk.com" target="_blank">suslusozluk.com</a>, a cosmetics forum with over 1m monthly visitors at the time, but an old code-base which couldn't handle the traffic anymore. I dug in and pushed my limits, re-doing and transitioning the complete website in less than a year. It now enjoys over 2.5m monthly users and I maintain it to this day.
       </v-card-text>
       <v-card-text class="text-h6">
-        After moving to Solingen, Germany in 2019 I started taking on projects from <a href="https://mammutmedia.eu" target="_blank">Mammut Media</a>, a German web & social media agency of young and enthusiastic entrepreneurs. I took mostly government work through them such as Solingen's job portal, touch-screen digital signage apps for their new outdoor screens, the backend of their city apps and the like.
+        In 2019 I moved to Germany and started taking on projects from <a href="https://mammutmedia.eu" target="_blank">Mammut Media</a>, a German web & social media agency of young and enthusiastic entrepreneurs. Work was mostly for the government of Solingen and nearby cities and it helped me have a stable job in Germany for my transition to living here.
       </v-card-text>
       <v-card-text class="text-h6">
-        During the COVID crisis, I also built several test-center and vaccination booking systems for other clients of Mammut Media.
+        During the COVID crisis, I built several test-center and vaccination booking systems for clients of Mammut Media, which were very successful and continue to be used vigorously to this day.
       </v-card-text>
       <v-card-text class="text-h6">
         I'm also building <a href="https://fireful.io" target="_blank">fireful.io</a>, a free plug & play content editor for firebase when I find time in between my other projects.
       </v-card-text>
       <v-card-text class="text-h6">
-        I'm looking to partner up as a developer or also an investor in any kind of project, but I'm mostly attracted to niche B2B SaaS ideas. Feel free to contact me via any of the platforms below:
+        I'm looking to partner up as a developer or also an investor in any kind of project, but I'm mostly attracted to niche B2B SaaS ideas. Feel free to contact me via <a href="https://www.linkedin.com/in/isokosan" target="_blank">linkedin</a>, my <a href="https://github.com/isokosan" target="_blank">github</a>, or you can find me at <a href="https://www.indiehackers.com/isokosan" target="_blank">indie hackers</a>.
       </v-card-text>
-      <v-card-actions>
-        <div class="d-flex flex-wrap justify-center justify-md-start">
-          <v-btn
-            text
-            class="mb-3"
-            href="https://github.com/isokosan"
-            target="_blank"
-          >
-            <v-icon large left>
-              {{ mdiGithub }}
-            </v-icon>
-            Github
-          </v-btn>
-          <v-btn
-            text
-            class="mb-3"
-            href="https://www.linkedin.com/in/isokosan"
-            target="_blank"
-          >
-            <v-icon large left>
-              {{ mdiLinkedin }}
-            </v-icon>
-            Linkedin
-          </v-btn>
-          <v-btn
-            text
-            class="mb-3"
-            href="https://www.indiehackers.com/isokosan"
-            target="_blank"
-          >
-            <v-avatar tile size="32px" class="mr-2">
-              <v-img contain src="./ih100.png" />
-            </v-avatar>
-            Indie Hackers
-          </v-btn>
-        </div>
-      </v-card-actions>
     </v-card>
 
     <!-- Projects -->
@@ -156,14 +119,14 @@
           </v-btn>
         </v-btn-toggle>
       </v-card-subtitle>
-      <v-card-text style="min-height: 400px;">
-        <div class="d-flex flex-wrap">
+      <v-card-text style="min-height: 400px; width: 100%;">
+        <div ref="isotope">
           <skill-chip
             v-for="skill in skills"
             :key="skill.name"
             :skill="skill"
-            class="mr-2 mb-2 d-none"
-            :class="{ 'd-flex': !selectedCategory || skill.category === selectedCategory }"
+            class="mr-2 mb-2 grid-item"
+            :class="`${skill.category}`"
           />
         </div>
       </v-card-text>
@@ -183,6 +146,8 @@ export default {
     return {
       filter: null,
       age: this.getAge('1989-05-01'),
+      isotope: null,
+      isotopeTimeout: null,
       mdiGithub,
       mdiLinkedin,
       mdiOpenInNew
@@ -191,11 +156,41 @@ export default {
   computed: {
     ...mapState(['portfolio']),
     ...mapGetters(['skills', 'categories']),
-    selectedCategory () {
+    filterCategory () {
       return this.filter >= 0
         ? this.categories[this.filter]
-        : null
+        : '*'
+    },
+    isotopeOptions () {
+      return {
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows',
+        // masonryHorizontal: {
+        //   rowHeight: 0
+        // },
+        filter: this.filterCategory
+      }
     }
+  },
+  watch: {
+    isotopeOptions () {
+      this.isotope.arrange({
+        // layoutMode: 'masonryHorizontal',
+        filter: itemElem => this.filterCategory === '*' || itemElem.classList.contains(this.filterCategory)
+      })
+    }
+  },
+  mounted () {
+    const fjs = document.getElementsByTagName('script')[0]
+    if (!document.getElementById('isotope-script')) {
+      const js = document.createElement('script')
+      js.id = 'isotope-script'
+      js.async = true
+      js.defer = true
+      js.src = 'https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js'
+      fjs.parentNode.insertBefore(js, fjs)
+    }
+    this.initIsotope()
   },
   methods: {
     getAge (dateString) {
@@ -207,6 +202,14 @@ export default {
         age--
       }
       return age
+    },
+    initIsotope () {
+      clearTimeout(this.isotopeTimeout)
+      if (window.Isotope) {
+        this.isotope = new window.Isotope(this.$refs.isotope, this.isotopeOptions)
+        return
+      }
+      this.isotopeTimeout = setTimeout(this.initIsotope, 100)
     }
   }
 }
